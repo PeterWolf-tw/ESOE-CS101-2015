ca4cae7c70afc46728911b588865ab193bb20d8a
PeterWolf <peterwolf.wang@gmail.com>
2015-10-08 17:07:19 +0800

    新增了一點示範

--- HelloAgain.py
+++ HelloAgain.py
@@ -6,3 +6,13 @@ print("Hello again!")
 
 
 print("哈哈耶我會用了")
+
+print("被刪圖好悲情啊…")
+
+
+def testPrinter(Something):
+    print(Something.upper())
+
+
+#if __name__ == "__main__":
+    #testPrinter("this is my book.")
