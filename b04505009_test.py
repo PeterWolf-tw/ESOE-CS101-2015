5cc850bc69c3ce36ebcf4d4da4e793f5dc9af5f2
b04505009 <b04505009@ntu.edu.tw>
2015-10-08 23:52:29 +0800

    只適用於最高位數字不為0 QQ

--- b04505009_test.py
+++ b04505009_test.py
@@ -1,27 +1,24 @@
 #!/usr/bin/env python 3
 #-*- coding:utf-8 -*-
-
-def int2bin(N):
-    '''
-    本函式將 int 整數轉為 bin 二進位制表示
-    '''
-    tmpLIST = []
-    while N > 0:
-        remainder = int(N % 2)
-        tmpLIST.append(remainder)
-        N = (N - remainder) / 2
-    tmpLIST.append(0)
-
-    ans = ""
-    for j in tmpLIST[::-1]: #將 tmpLIST 中的數字從尾至頭傳入 j
-        ans = ans + str(j)
-    print("{0} 的二進位表示為 {1}.".format(intNumber, ans))
-    return None
-
-if __name__ == '__main__':
-    intNumber = 100
     
-    int2bin(intNumber)
+def bin2int(N):
+    '''
+    本函式將 bin 二進位制表示數轉為 int 整數
+    '''
+    mod = 0
+    x = 0
+    i = 0
+    a = 0
+    while N > 0: 
+        mod = int(N % 10)
+        b = 2 ** i
+        a =  a + mod*b
+        i = i + 1
+        N = (N - mod) / 10 
+    print("{0} 的十進位表示為 {1}.".format(binNumber, a))
+if __name__ == '__main__':
+    binNumber = 1100100
+        
+    bin2int(binNumber)    
+    
 
-    binNumber = "01100101"
-    #bin2int(number)
