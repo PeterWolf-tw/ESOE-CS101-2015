d425b8fa31d6913c71302c65ad601123c9e52504
panyan928 <ananan928@outlook.com>
2015-10-11 17:45:52 +0800

    all done

--- T04505201_HW02.py
+++ T04505201_HW02.py
@@ -25,14 +25,26 @@ def bin2int(N):
     for i in range(length):
         n+=number%10*(2**i)
         number=int(number/10)
-    print ("{0} 的二進位表示為 {1}.".format(N, n))
+    print ("{0} 的十進位表示為 {1}.".format(N, n))
     return None
 
 if __name__ == '__main__':
-    intNumber = 100
+    intNumber = 560
     int2bin(intNumber)
     binNumber = "01100101"
     bin2int(binNumber)
+    print("please input another binary number:")
+    num=input()
+    bin2int(num)
+    
+#2-19:a. 10位 b. 17位 c. 6位  d. 8位
+#2-20:a. 14位 b. 8位  c. 13位 d. 4位
+#2-22:a. 00010001 11101010 00100010 00001110
+#     b. 00001110 00111000 11101010 00111000
+#     c. 01101110 00001110 00111000 01001110
+#     d. 00011000 00111000 00001101 00001011
+#3-28: a.234  b.560  c.874  d.888
+#3-30: a.234  b.560  c.875  d.889
 
 
 
