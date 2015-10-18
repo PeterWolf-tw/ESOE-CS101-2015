843a7220fa7d76cf8a362a08e4d28591c171aa7c
babayazz <b04505022@ntu.edu.tw>
2015-10-18 23:52:19 +0800

    助教請進!

---
+++ B04505022_HW02.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+def bin2int(N):
+    '''
+    本函式將 bin 二進位制表示數轉為 int 整數
+    '''
+   
+    
+    a = 1
+    ans = 0
+    while (N > 0):
+        reminder = N % 10
+        if (reminder > 0):
+            ans = reminder*a
+        a = a*2
+            
+        
+    return ans
+    
+
+
+if __name__ == '__main__':
+    binNumber = 100
+    bin2int(binNumber)
+    print(bin2int(binNumber))    
+    
+    
+    
+#2
+#a = 10
+#b = 17
+#c = 6
+#d = 8
+#3
+#a = 14
+#b = 8
+#c = 13
+#d = 4
+#4
+#a = 00010001 11101010 00100010 00001110
+#b = 00001110 00111000 11101010 00111000
+#c = 01101110 00001110 00111000 01001110
+#d = 00011000 00111000 00001101 00001011
+#5
+#a = 234
+#b = 560
+#c = 874
+#d = 888
+#6
+#a = 234
+#b = 560
+#c = 875
+#d = 889        
