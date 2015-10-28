4a88242e44c8571d65ce19f5eda55aa98143ef94
PeterWolf <peterwolf.wang@gmail.com>
2015-10-28 15:25:03 +0800

    公佈作業；繳交期限至 10/5 為止。

---
+++ Assignment_HW04.py
@@ -0,0 +1,74 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#請參考以下 condNOT() 的例子，設計四個 func() 依以下條件，能算出 condition02 ~ 04 的值
+
+#condition00 not condition01
+def condNOT(inputSTR_X):
+    outputSTR = ""
+    for i in inputSTR_X:
+        if i == "0":
+            outputSTR = outputSTR + "1"
+        else:
+            outputSTR = outputSTR + "0"
+    return outputSTR
+
+
+#condition00 and condition02
+def condAND(inputSTR_X, inputSTR_Y):
+    return
+
+#condition00 or condition03
+def condOR(inputSTR_X, inputSTR_Y):
+    return
+
+#condition00 xor condition04
+def conXOR(inputSTR_X, inputSTR_Y):
+    return
+
+
+
+def hex2Bin(hexSTR):
+    '''
+    16 進位表示式轉為 2 進位表示式
+    '''
+    return bin(int(hexSTR, 16))[2:]
+
+
+if __name__== "__main__":
+    condition00X = "010111001010100001100011"
+    condition00Y = "010000110001011100101001"
+
+    condition01 = condNOT(condition00X)
+    #print(condition01)
+
+    #可利用 hex2Bin() 函式計算十六進位表示式 "99" 的二進位表式：(範例如下)
+    b = hex2Bin("99")
+    print(b)
+
+
+    print("Ans:")
+    Ch4P4_3a = ""
+    Ch4P4_3b = ""
+    Ch4P4_3c = ""
+    Ch4P4_3d = ""
+    print("========")
+    Ch4P4_4a = ""
+    Ch4P4_4b = ""
+    Ch4P4_4c = ""
+    Ch4P4_4d = ""
+    print("========")
+    Ch4P4_13a = ""
+    Ch4P4_13b = ""
+    Ch4P4_13c = ""
+    Ch4P4_13d = ""
+    print("========")
+    Ch4P4_15a = ""
+    Ch4P4_15b = ""
+    Ch4P4_15c = ""
+    Ch4P4_15d = ""
+    print("========")
+    Ch4P4_16a = ""
+    Ch4P4_16b = ""
+    Ch4P4_16c = ""
+    Ch4P4_16d = ""
