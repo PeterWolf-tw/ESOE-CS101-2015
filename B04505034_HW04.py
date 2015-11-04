a76beb41de9ee05890717f786cdef4c104944036
Karen0504 <gm.lu.karen@gmail.com>
2015-11-05 00:50:17 +0800

    homework04

--- B04505034_HW04.py
+++ B04505034_HW04.py
@@ -59,34 +59,65 @@ if __name__== "__main__":
 
     condition01 = condNOT(condition00X)
     print(condition01)
+    condition02 = condAND(condition00X, condition00Y)
+    print(condition02)
+    condition03 = condOR(condition00X, condition00Y)
+    print(condition03)
+    condition04 = conXOR(condition00X, condition00Y)
+    print(condition04)
 
     #可利用 hex2Bin() 函式計算十六進位表示式 "99" 的二進位表式：(範例如下)
     b = hex2Bin("99")
     print(b)
+    c = hex2Bin("00")
+    print(c)
+    d = hex2Bin("FF")
+    print(d)
+    e = hex2Bin("33")
+    print(e)
 
 
     print("Ans:")
-    Ch4P4_3a = ""
-    Ch4P4_3b = ""
-    Ch4P4_3c = ""
-    Ch4P4_3d = ""
+    Ch4P4_3a = condOR(b, b)
+    print(Ch4P4_3a)
+    Ch4P4_3b = "10011001" #condOR(b, c)
+    print(Ch4P4_3b)
+    Ch4P4_3c = condOR(b, d)
+    print(Ch4P4_3c)
+    Ch4P4_3d = condOR(d, d)
+    print(Ch4P4_3d)
     print("========")
-    Ch4P4_4a = ""
-    Ch4P4_4b = ""
-    Ch4P4_4c = ""
-    Ch4P4_4d = ""
+    Ch4P4_4a = condNOT(condOR(b ,b))
+    print(Ch4P4_4a)
+    Ch4P4_4b = "11111111"     #condOR(b, condNOT(c))
+    print(Ch4P4_4b)
+    Ch4P4_4c = "00010001"     #condOR(condAND(b, e), condAND(c, d))
+    print(Ch4P4_4c)
+    Ch4P4_4d = "10111011"     #condAND(condOR(b, e), condOR(c, d))
     print("========")
-    Ch4P4_13a = ""
-    Ch4P4_13b = ""
-    Ch4P4_13c = ""
-    Ch4P4_13d = ""
+    Ch4P4_13a = "1184"
+    print(Ch4P4_13a)
+    Ch4P4_13b = "-62"
+    print(Ch4P4_13b)
+    Ch4P4_13c = "862"
+    print(Ch4P4_13c)
+    Ch4P4_13d = "-1184"
+    print(Ch4P4_13d)
     print("========")
-    Ch4P4_15a = ""
-    Ch4P4_15b = ""
-    Ch4P4_15c = ""
-    Ch4P4_15d = ""
+    Ch4P4_15a = "-119 =>overflow"
+    print(Ch4P4_15a)
+    Ch4P4_15b = "-73"
+    print(Ch4P4_15b)
+    Ch4P4_15c = "73"
+    print(Ch4P4_15c)
+    Ch4P4_15d = "119 =>overflow"
+    print(Ch4P4_15d)
     print("========")
-    Ch4P4_16a = ""
-    Ch4P4_16b = ""
-    Ch4P4_16c = ""
-    Ch4P4_16d = ""
+    Ch4P4_16a = "0F51"
+    print(Ch4P4_16a)
+    Ch4P4_16b = "0F2A"
+    print(Ch4P4_16b)
+    Ch4P4_16c = "8012"
+    print(Ch4P4_16c)
+    Ch4P4_16d = "7F51"
+    print(Ch4P4_16d)
