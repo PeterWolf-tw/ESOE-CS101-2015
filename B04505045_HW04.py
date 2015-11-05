af5a2a6fd0fd4529d02c2024d53d9263ae15879a
b04505045 <b04505045@ntu.edu.tw>
2015-11-05 20:55:18 +0800

    不會補零QQ

--- B04505045_HW04.py
+++ B04505045_HW04.py
@@ -17,7 +17,7 @@ def condNOT(inputSTR_X):
 #condition00 and condition02
 def condAND(inputSTR_X, inputSTR_Y):
     outputSTR = ""
-    for a in inputSTR_X and b in inputSTR_Y:
+    for (a,b) in zip(inputSTR_X,inputSTR_Y): 
         if a == "1" and b == "1":
             outputSTR = outputSTR + "1"
         else:
@@ -28,7 +28,7 @@ def condAND(inputSTR_X, inputSTR_Y):
 #condition00 or condition03
 def condOR(inputSTR_X, inputSTR_Y):
     outputSTR = ""
-    for a in inputSTR_X and b in inputSTR_Y:
+    for (a,b) in zip(inputSTR_X,inputSTR_Y):
         if a == "0" and b == "0":
             outputSTR = outputSTR + "0"
         else:
@@ -37,11 +37,81 @@ def condOR(inputSTR_X, inputSTR_Y):
     
 
 #condition00 xor condition04
-def conXOR(inputSTR_X, inputSTR_Y):
+def condXOR(inputSTR_X, inputSTR_Y):
     outputSTR = ""
-    for a in inputSTR_X and b in inputSTR_Y:
+    for (a,b) in zip(inputSTR_X,inputSTR_Y):
         if a == b:
             outputSTR = outputSTR + "0"
         else:
             outputSTR = outputSTR + "1"
     return outputSTR 
+
+
+def hex2Bin(hexSTR):
+    '''
+    16 進位表示式轉為 2 進位表示式
+    '''
+    return bin(int(hexSTR, 16))[2:]
+
+
+if __name__ == "__main__":
+    condition00X = "010111001010100001100011"
+    condition00Y = "010000110001011100101001"    
+    
+    condition01 = condNOT(condition00X)
+    print(condition01)
+    condition02 = condAND(condition00X, condition00Y)
+    print(condition02)
+    condition03 = condOR(condition00X, condition00Y)
+    print(condition03)
+    condition04 = condXOR(condition00X, condition00Y)
+    print(condition04)
+    
+    #可利用 hex2Bin() 函式計算十六進位表示式 "99" 的二進位表式：(範例如下)
+    b = hex2Bin("99")
+    print("b(99) = ",b)
+    c = hex2Bin("FF")
+    print("c(FF) = ",c)
+    d = hex2Bin("00")
+    print("d(00) = ",d)
+    e = hex2Bin("33")
+    print("e(33) = ",e)
+    print("========")
+    
+    
+    print("Ans:")
+    Ch4P4_3a = condOR(b,b)
+    print("Ch4P4_3a = ",Ch4P4_3a)
+    Ch4P4_3b = condOR(b,d)
+    print("Ch4P4_3b = ",Ch4P4_3b)
+    Ch4P4_3c = condOR(b,c)
+    print("Ch4P4_3c = ",Ch4P4_3c)
+    Ch4P4_3d = condOR(c,c)
+    print("Ch4P4_3d = ",Ch4P4_3d)
+    print("========")
+    
+    Ch4P4_4a = condNOT(condOR(b,b))
+    print("Ch4P4_4a = ",Ch4P4_4a)
+    Ch4P4_4b  = condOR(b,condNOT(d))
+    print("Ch4P4_4b = ",Ch4P4_4b)
+    Ch4P4_4c = condOR(condAND(b,e),condAND(d,c))
+    print("Ch4P4_4c = ",Ch4P4_4c)
+    Ch4P4_4d = condAND(condOR(b,e),condOR(d,c))
+    print("Ch4P4_4d = ",Ch4P4_4d)
+    print("========")
+    print("Ch4P4_13a = 0000 0100 1010 0000 ( 1184)" )
+    print("Ch4P4_13b = 1111 1100 1010 0010 (-862 )" )
+    print("Ch4P4_13c = 0000 0011 0101 1110 ( 862 )"  )
+    print("Ch4P4_13d = 1111 1011 0110 0000 (-1184)")
+        
+    print("========")
+    print("Ch4P4_15a = 1000 1001 (-119(overflow)   )")
+    print("Ch4P4_15b = 1011 0111 (-73(non-overflow))")
+    print("Ch4P4_15c = 0100 1001 ( 73(non-overflow))")
+    print("Ch4P4_15d = 0111 0111 ( 119(overflow)   )")
+        
+    print("========")
+    print("Ch4P4_16a = 0000 1111 0101 0001  (0F51)")
+    print("Ch4P4_16b = 0000 1111 0010 1010  (0F2A)")
+    print("Ch4P4_16c = 1000 0000 0001 0010  (8012)")
+    print("Ch4P4_16d = 0111 1111 0101 0001  (7F51(overflow))")
