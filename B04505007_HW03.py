b24668eefe279ba6e2f38beaf6dcbdade6e37c74
Kevin Chen <kevinchen@KevindeMacBook-Pro.local>
2015-11-25 17:50:45 +0800

    wo hoa shuan o

---
+++ B04505007_HW03.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+
+
+
+def charFreqLister(inputSTR):
+    resultLIST = []
+    
+
+
+    
+    for x in inputSTR:
+        c = inputSTR.count(i)
+        if (x,c/len(inputSTR)) not in resultLIST:
+            resultLIST.append((x,c/len(inputSTR)))
+    resultLIST.sort( key = lambda x : x[1], reverse=True)
+        
+    return resultLIST
+
+        
+
+              
+               
+
+if __name__== "__main__":
+output = charFreqLister(input("kevin handsome boy"))    
+    print(output)
+
+
+
+        
