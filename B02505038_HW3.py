481cb998425d7fb597624e51844629ac2bd11aec
YOUR NAME <YOUR EMAIL ADDRESS>
2015-11-25 13:40:45 +0800

    隨便打

--- B02505038_HW3.py
+++ B02505038_HW3.py
@@ -1,24 +1,24 @@
-+#!/usr/bin/env python3
-+# -*- coding:utf-8 -*-
-+
-+def charfreqlister(str):
-+    len_str = len(str)
-+    
-+    result = []
-+    for c in str:
-+        is_inside = False
-+        for element in result:
-+            if element[1] == c:
-+                element[0] += 1
-+                is_inside = True
-+                break
-+        if not is_inside:
-+            result.append([1, c])
-+            
-+    for element in result:
-+        element[0] /= len_str
-+
-+    return result
-+
-+if __name__ == "__main__":
-+    print(charfreqlister("爽"))
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+
+def charfreqlister(str):
+    len_str = len(str)
+
+    result = []
+    for c in str:
+        is_inside = False
+        for element in result:
+            if element[1] == c:
+                element[0] += 1
+                is_inside = True
+                break
+        if not is_inside:
+            result.append([1, c])
+            
+    for element in result:
+        element[0] = float(element[0]) / len_str
+
+    return result
+
+if __name__ == "__main__":
+    print(charfreqlister("Tony"))
