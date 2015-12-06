bd1aa13f8175b375b34370f5d387b38a980180a6
josephcandy <joseph860517@gmail.com>
2015-12-06 21:32:42 +0800

    最小方差

---
+++ Assignment_Team05_HW05.py
@@ -0,0 +1,12 @@
+#This is question designed by us.
+#It's not very hard.
+#It is about the linear least squares.
+#We will give a bunch of xdata and ydata.
+#Of course, the number of them are equal.
+#The only thing you have to do is to give the equation in the form of "y = ax + b"
+#Don't use anything such as numpy, scipy or what.
+#I can't test the program without the package.
+#Don't try to connect to the Wolframalpha or Matlab.
+#I will test it without internet.
+#You can surely copy other classmates' codes, but make sure each other's willingness.
+#Or your codes will be regard as copy one, though you will still get the points.
