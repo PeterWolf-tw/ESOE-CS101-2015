bac6cb6b47457f3780ac3ff3aa4d05e4d058e091
ChenTsaiShien <B04505025@ntu.edu.tw>
2015-12-06 22:50:17 +0800

    將任意進位法轉換成其他的進位法

---
+++ Assignment_Team12_HW05.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#請參考以下空白程式，能夠將輸入的字串定義為m進位法，並且換算成n進位法
+#其中，m及n為任意輸入的數值，且是介於2至36的正整數
+
+def m2n(x):
+    '''
+    本函式將 m 進位制表示數轉為 n 進位制
+    '''    
+    a = input("m = ")
+    b = input("n = ")
+      
+if __name__ == "__main__":
+   
+    testSTR = "16ZEQBA"
+    result = m2n(testSTR)
+    print(result)
