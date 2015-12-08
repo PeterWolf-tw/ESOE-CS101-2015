c33836c351ee9b94be87037449fbe61bc82cdad8
josephcandy <joseph860517@gmail.com>
2015-12-08 09:51:38 +0800

    修改版

--- Assignment_Team05_HW05.py
+++ Assignment_Team05_HW05.py
@@ -10,3 +10,8 @@
 #I will test it without internet.
 #You can surely copy other classmates' codes, but make sure each other's willingness.
 #Or your codes will be regard as copy one, though you will still get the points.
+
+#!xdata and ydata are both lists
+#!for example, xdata=[1,6,8,4,5,2,3,5], ydata=[4,9,3,5,4,2,6,7]
+#!we want the result in string "y = 0.2535x + 3.9225"
+#!for our convenience, please use  if __name__== "__main__":
