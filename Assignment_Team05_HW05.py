6c3f489e4836236d1c5590573731d8c6925e6c16
jftyjfty3 <jftyjfty3@gmail.com>
2015-12-08 09:57:09 +0800

    revised with example

--- Assignment_Team05_HW05.py
+++ Assignment_Team05_HW05.py
@@ -1,17 +1,18 @@
-#This is question designed by us.
-#It's not very hard.
-#It is about the linear least squares.
-#We will give a bunch of xdata and ydata.
-#Of course, the number of them are equal.
-#The only thing you have to do is to give the equation in the form of "y = ax + b"
-#Don't use anything such as numpy, scipy or what.
-#I can't test the program without the package.
-#Don't try to connect to the Wolframalpha or Matlab.
-#I will test it without internet.
-#You can surely copy other classmates' codes, but make sure each other's willingness.
-#Or your codes will be regard as copy one, though you will still get the points.
+This is question designed by us.
+It's not very hard.
+!It is about the linear least squares.
+We will give a bunch of xdata and ydata.
+!The only thing you have to do is to give the equation in the form of "y = ax + b" by using linear least squares method
+!AS for the number of data is determined by us, so you can use while loop or for loop with range
+Don't use anything such as numpy, scipy or whatever.
+I can't test the program without the package.
+Don't try to connect to the Wolframalpha or Matlab.
+I will test it without internet.
+You can surely copy other classmates' codes, but make sure each other's willingness.
+Or your codes will be regard as copy one, though you will still get the points.
 
-#!xdata and ydata are both lists
-#!for example, xdata=[1,6,8,4,5,2,3,5], ydata=[4,9,3,5,4,2,6,7]
-#!we want the result in string "y = 0.2535x + 3.9225"
-#!for our convenience, please use  if __name__== "__main__":
+!xdata and ydata are both lists
+!for example, xdata=[1,6,8,4,5,2,3,5], ydata=[4,9,3,5,4,2,6,7]
+!we want the result in string "y = 0.2535x + 3.9225"
+!for our convenience, please use  if __name__== "__main__":
+! MIND IT ! THE PERCISION NEED TO BE AT LEAST E-4(10^-4)
