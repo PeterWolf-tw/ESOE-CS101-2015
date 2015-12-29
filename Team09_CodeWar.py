dacfbc89a9d4b1542f1b8df3a74a0ff1c720ec1b
brandon1210 <brandon851210@hotmail.com>
2015-12-29 16:55:02 +0800

    ya

--- Team09_CodeWar.py
+++ Team09_CodeWar.py
@@ -8,31 +8,31 @@
 #例如輸入633
 #可得1 3 211 633
 
+def team01():
+    x=int(input("please enter a number: "))
+    #print (x)
+    LIST1 = []
 
-x=int(input("please enter a number: "))
-#print (x)
-LIST1 = []
+    import math
+    a = int(math.sqrt(x))
+    #print (a)
 
-import math
-a = int(math.sqrt(x))
-#print (a)
-
-i=1
-for i in range(1, a+1) :
-    if x % i == 0:
-        LIST1.append(i)
-        if x//i not in LIST1:
-            LIST1.append(x//i)
+    i=1
+    for i in range(1, a+1) :
+        if x % i == 0:
+            LIST1.append(i)
+            if x//i not in LIST1:
+                LIST1.append(x//i)
         
-        #print (LIST1)
-        i = i+1
-        continue
-    else:
-        i = i+1
-        continue
-LIST1.sort(reverse=False)
+            #print (LIST1)
+            i = i+1
+            continue
+        else:
+            i = i+1
+            continue
+    LIST1.sort(reverse=False)
 
-print (LIST1)
+    print (LIST1)
 
 
 
@@ -43,21 +43,21 @@ print (LIST1)
 #可算出費式數列第n項的值
 #費式數列第n項的值等於第(n-1)項與第(n-2)項的和
 #且a1=a2=1
+def team04():
+    LIST2 = []
+    LIST2.insert(0,1)
+    LIST2.insert(1,1)
+    LIST2.insert(2,2)
 
-LIST2 = []
-LIST2.insert(0,1)
-LIST2.insert(1,1)
-LIST2.insert(2,2)
+    n=int(input("please enter a number: "))
 
-n=int(input("please enter a number: "))
-
-while n > 3 :
-    LIST2.append(LIST2[-1]+LIST2[-2])
-    if len(LIST2)==n:
-        break
-    if n < 3 :
-        LIST2 = [1,1,2]
-print (LIST2[n-1])
+    while n > 3 :
+        LIST2.append(LIST2[-1]+LIST2[-2])
+        if len(LIST2)==n:
+            break
+        if n < 3 :
+            LIST2 = [1,1,2]
+    print (LIST2[n-1])
 
 
 
@@ -65,34 +65,35 @@ print (LIST2[n-1])
 #請輕鬆設計出一個可計算出任意個實數組之表準差的函數
 # ex: input=(40,50,60) , output=8.164965809277....
 # ex: input=(60,60,60,60) , output=0
-LIST3 = []
-import math
-LIST3 = [x for x in input("give me some numbers:").split()]
-print (LIST3)
-average = sum(LIST3)/len(LIST3)
-for n in range (0,len(LIST3)-1):
-    LIST4 = []
-    LIST4.insert(n,LIST3[n] - average)
-    LIST5 = []
-    LIST5.insert(n , LIST4[n] * LIST4[n])
-    answer = math.sqrt(sum(LIST5) / len(LIST5))
+''''def team06():
+    LIST3 = []
+    import math
+    LIST3 = [x for x in input("give me some numbers:").split()]
+    print (LIST3)
+    average = sum(LIST3)/len(LIST3)
+    for n in range (0,len(LIST3)-1):
+        LIST4 = []
+        LIST4.insert(n,LIST3[n] - average)
+        LIST5 = []
+        LIST5.insert(n , LIST4[n] * LIST4[n])
+        answer = math.sqrt(sum(LIST5) / len(LIST5))
     print (answer)
 
-
+'''
 
 #Team09
-import random
-LIST = [0,1,2,3,4,5,6,7,8,9]
-print (random.sample (LIST , 4))
-
-x = 0
 
 def inputTest():
+    import random
+    LIST = [0,1,2,3,4,5,6,7,8,9]
+    print (random.sample (LIST , 4))
+    ans = random.sample (LIST , 4)
+    x = 0    
     x = input(">>> Input: ")
     print (x)
 
-while (True):
-    inputTest()
+    while (True):
+        inputTest()
     
 # 請設計一個程式能自動列出九九乘法表如下(每行要對齊):
     
@@ -119,12 +120,41 @@ def team10():
         print('')
       
     return ''
+
+def team14(n):
+    import random
+    
+    for i in range(0,n):    
+        j=0
+        resultLIST =[]
+        while(j != 6):
+            a = random.randint(1,49)
+            if a not in resultLIST:
+                resultLIST.append(a)
+                j += 1
+        resultLIST.sort()
+        print(resultLIST)
+
     
 if __name__== "__main__":
+    print("team01:")
+    t01 = team01()
+    print(t01)
+    
+    print("team04:")
+    t04 = team04()
+    print(t04)
+    
+    #print("team06:")
+    #t06 = team06()
+    #print(t06)
+    
     print("team10:")
     t10 = team10()
-    print(t10)
+    print(t10)    
     
+    print("team14:")
+    print(team14(int(input("please give me a number:"))))
     
     
     
