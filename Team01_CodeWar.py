88d0f07b9987308be5be5b493ff6c1c6b02a6c9a
b04505045 <b04505045@ntu.edu.tw>
2015-12-29 14:31:03 +0800

    一題了耶!

---
+++ Team01_CodeWar.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-2
+def jjj(x):
+    y=list(x)
+    y.append(y[0])
+    y.remove(y[0])
+    y.append("ay")
+    z = ""
+    for i in y :
+        z = z + i
+    return z
+if __name__ == '__main__':
+    a=input("請輸入一個英文單字")
+    print(jjj(a)
