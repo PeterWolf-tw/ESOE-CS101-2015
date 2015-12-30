e963d744575c1ad9ff55702493f48a4fc709aecb
brandon1210 <brandon851210@hotmail.com>
2015-12-31 00:47:25 +0800

    To TA,這樣寫程式其實還蠻好玩的耶

--- Team09_CodeWar.py
+++ Team09_CodeWar.py
@@ -415,100 +415,133 @@ def team13():
     rhaegar = ["Rhaegar Targargen",700,000,"t2",0]
     tywin = ["Tywin Lannister",500,500,"t3",2]
     jamie = ["Jamie Lannister",600,500,"t3",3]
-    A=input("Who?")
-    B=input("And who?")
-    x = []
-    x.append(A)
-    y = []
-    y.append(B)
-    X =[]
-    Y =[]
+    
     #print(x)
     #print(y)
-    for a,b,c,d,e,f,g,h,i in zip(jon,robert,eddard,ser,bran,hodor,rhaegar,tywin,jamie):
-        if x[0]==jon[0]:
-            X=jon
-        if x[0]==robert[0]:
-            X=robert
-        if x[0]==eddard[0]:
-            X=eddard    
-        if x[0]==ser[0]:
-            X=ser        
-        if x[0]==rhaegar[0]:
-            X=rhaegar        
-        if x[0]==bran[0]:
-            X=bran
-        if x[0]==hodor[0]:
-            X=hodor                            
-        if x[0]==tywin[0]:
-            X=tywin                            
-        if x[0]==jamie[0]:
-            X=jamie       
-        if y[0]==jon[0]:
-            Y=jon
-        if y[0]==robert[0]:
-            Y=robert
-        if y[0]==eddard[0]:
-            Y=eddard    
-        if y[0]==ser[0]:
-            Y=ser        
-        if y[0]==rhaegar[0]:
-            Y=rhaegar        
-        if y[0]==bran[0]:
-            Y=bran
-        if y[0]==hodor[0]:
-            Y=hodor                            
-        if y[0]==tywin[0]:
-            Y=tywin                            
-        if y[0]==jamie[0]:
-            Y=jamie
-        if X==[] :
-            print("You have the wrong name")
-        if Y==[] :
-            print("You have the wrong name")            
+    k=1
+    j=1
+    while k==1 or j==1:
+        A=input("Who?")
+        B=input("And who?")
+        x = []
+        x.append(A)
+        y = []
+        y.append(B)
+        X =[]
+        Y =[]        
+        for a,b,c,d,e,f,g,h,i in zip(jon,robert,eddard,ser,bran,hodor,rhaegar,tywin,jamie):
+            if x[0]==jon[0]:
+                X=jon
+                j=2
+            if x[0]==robert[0]:
+                X=robert
+                j=2
+            if x[0]==eddard[0]:
+                X=eddard 
+                j=2
+            if x[0]==ser[0]:
+                X=ser 
+                j=2
+            if x[0]==rhaegar[0]:
+                X=rhaegar 
+                j=2
+            if x[0]==bran[0]:
+                X=bran
+                j=2
+            if x[0]==hodor[0]:
+                X=hodor  
+                j=2
+            if x[0]==tywin[0]:
+                X=tywin 
+                j=2
+            if x[0]==jamie[0]:
+                X=jamie  
+                j=2
+            if y[0]==jon[0]:
+                Y=jon
+                k=2
+            if y[0]==robert[0]:
+                Y=robert
+                k=2
+            if y[0]==eddard[0]:
+                Y=eddard    
+                k=2
+            if y[0]==ser[0]:
+                Y=ser   
+                k=2
+            if y[0]==rhaegar[0]:
+                Y=rhaegar   
+                k=2
+            if y[0]==bran[0]:
+                Y=bran
+                k=2
+            if y[0]==hodor[0]:
+                Y=hodor  
+                k=2
+            if y[0]==tywin[0]:
+                Y=tywin  
+                k=2
+            if y[0]==jamie[0]:
+                Y=jamie
+                k=2
+            if X==[] :
+                print("You have the wrong name")
+                
+            if Y==[] :
+                print("You have the wrong name")
+               
     #print(X)
     #print(Y)
     if X[1]==Y[1]:
-        print("Stop Kidding Me! Asshole!!!")    
+        print("Stop Kidding Me! Asshole!!!")
+        return team13()
     elif X[1]==Y[2]:
         print(A,end=" ")
         print("是",end=" ")
         print(B,end=" ")
-        print("的長官") 
+        print("的長官")
+        return team13()
     elif X[2]==Y[1]:
         print(A,end=" ")
         print("是",end=" ")
         print(B,end=" ")
-        print("的屬下")       
+        print("的屬下")
+        return team13()
     elif X[2]==Y[2]:
         print(A,end=" ")
         print("與",end=" ")
         print(B,end=" ")
         print("是平輩")
+        return team13()
     elif X[3]!=Y[3]:
         print(A,end=" ")
         print("是",end=" ")
         print(B,end=" ")
         print("的敵人")
+        return team13()
     elif X[3]==Y[3]:
         if X[4]==1:
             print(A,end=" ")
             print("是",end=" ")
             print(B,end=" ")
             print("的長官")
+            return team13()
         elif Y[4]==1:
             print(A,end=" ")
             print("是",end=" ")
             print(B,end=" ")
-            print("的屬下")             
+            print("的屬下")
+            return team13()
         elif X[1]!=Y[2]:
             if X[2]!=Y[1]:
                 print(A,end=" ")
                 print("與",end=" ")
                 print(B,end=" ")
-                print("是平輩")         
+                print("是平輩")
+                return team13()
     else:
         print("Stop Kidding Me! Asshole!!!")
+        return team13()
 
 
 #Team14
@@ -529,23 +562,23 @@ def team14(n):
 
     
 if __name__== "__main__":
-    print("team01:"
-          "設計一個程式"
-          "若使用者輸入一個數值"
-          "可算出該數值所具有的所有因數")
+    print("team01:")
+    print("設計一個程式")
+    print("若使用者輸入一個數值")
+    print("可算出該數值所具有的所有因數")
     t01 = team01()
     print(t01)
     
-    print("team02:"
-          "Pig Latin is a language game"
-          "where you move the first letter of the word to the end" 
-          "and add ay. "
-          "So Python becomes ythonpay.")
+    print("team02:")
+    print("Pig Latin is a language game")
+    print("where you move the first letter of the word to the end") 
+    print("and add ay. ")
+    print("So Python becomes ythonpay.")
     print(team02())
     
-    print("team03:"
-          "針對一個class Friends"
-          "定義names 和 connected")
+    print("team03:")
+    print("針對一個class Friends")
+    print("定義names 和 connected")
     friends = Friends(({"a", "b"}, {"b", "c"}, {"c", "a"}))
     friends2 = Friends(({"1", "2"}, {"2", "4"}, {"1", "3"}))
     print(friends.names())
@@ -554,78 +587,77 @@ if __name__== "__main__":
     print(friends2.names())
     print(friends2.connected("1"))    
     
-    print("team04:"
-          "設計一個程式"
-          "若input=n"
-          "可算出費式數列第n項的值"
-          "費式數列第n項的值等於第(n-1)項與第(n-2)項的和"
-          "且第一項a1=第二項a2=1")
+    print("team04:")
+    print("設計一個程式")
+    print("若input=n")
+    print("可算出費式數列第n項的值")
+    print("費式數列第n項的值等於第(n-1)項與第(n-2)項的和")
+    print("且第一項a1=第二項a2=1")
     t04 = team04()
     print(t04)
     
-    print("team05:"
-          "It is about the linear least squares."
-          "We will give a bunch of xdata and ydata."
-          "The only thing you have to do is to give the equation in the form of y = ax + b by using linear least squares method"
-          "AS for the number of data is determined by us, so you can use while loop or for loop with range"
-          "xdata and ydata are both lists")
+    print("team05:")
+    print("It is about the linear least squares.")
+    print("We will give a bunch of xdata and ydata.")
+    print("The only thing you have to do is to give the equation in the form of y = ax + b by using linear least squares method")
+    print("AS for the number of data is determined by us, so you can use while loop or for loop with range")
+    print("xdata and ydata are both lists")
     t05 = team05()
     print(t05)
     
-    print("team06:"
-          "請輕鬆設計出一個可計算出任意個實數組之標準差的函數")
+    print("team06:")
+    print("請輕鬆設計出一個可計算出任意個實數組之標準差的函數")
     t06 = team06()
     print(t06)
     
-    print("team07:"
-          "設計一支程式，使其在輸入一串互不相等數字之後，輸出其所有可能之排列個數及排列。"
-          "1.首先輸入一個正整數 n ( 0 < n < 11 )，代表接下來要輸入的數字個數。"
-          "2.接著輸入 n 個正整數 a1, a2, a3,....an ( ai != aj, where 1 < i, j < 11 and i != j )。")
+    print("team07:")
+    print("設計一支程式，使其在輸入一串互不相等數字之後，輸出其所有可能之排列個數及排列。")
+    print("1.首先輸入一個正整數 n ( 0 < n < 11 )，代表接下來要輸入的數字個數。")
+    print("2.接著輸入 n 個正整數 a1, a2, a3,....an ( ai != aj, where 1 < i, j < 11 and i != j )。")
     print (team0701())
     print (team0702())
     
-    print("team09:"
-          "試設計一個可以猜數字的程式"
-          "幾A幾B")
+    print("team09:")
+    print("試設計一個可以猜數字的程式")
+    print("幾A幾B")
     print(random())
     
     print("team10:")
     t10 = team10()
     print(t10)
     
-    print ("team11:"
-           "猜數字遊戲"
-           "參賽者2~5人,先各自輸入名字,電腦隨機擇一範圍在1~36內的數字,並隨機擇一玩家可知曉該數字,開始遊戲."
-           "各玩家按順序喊+1~+3,除知曉數字的人以外各玩家可使用功能牌:pass or return一次,持續到有一玩家喊到或經過設定的數字者為輸.可重複遊戲."
-           "需該回合玩家知道她可輸入得指令 e.x: 玩家___,你可輸入數字1~3 or pass or return")
+    print ("team11:")
+    print("猜數字遊戲")
+    print("參賽者2~5人,先各自輸入名字,電腦隨機擇一範圍在1~36內的數字,並隨機擇一玩家可知曉該數字,開始遊戲.")
+    print("各玩家按順序喊+1~+3,除知曉數字的人以外各玩家可使用功能牌:pass or return一次,持續到有一玩家喊到或經過設定的數字者為輸.可重複遊戲.")
+    print("需該回合玩家知道她可輸入得指令 e.x: 玩家___,你可輸入數字1~3 or pass or return")
     print(rrandom())
     
     
-    print("team12:"
-          "請參考以下空白程式，設計一支程式能將輸入的字串定義為m進位法，並且換算成n進位法(其中，m, n為介於2至61的正整數)"
-          "在此，我們定義大寫英文字母A為10、Z為35;小寫英文字母a為36、z為61，其他字母以此類推"
-          "可不考慮負數及浮點數的計算")
+    print("team12:")
+    print("請參考以下空白程式，設計一支程式能將輸入的字串定義為m進位法，並且換算成n進位法(其中，m, n為介於2至61的正整數)")
+    print("在此，我們定義大寫英文字母A為10、Z為35;小寫英文字母a為36、z為61，其他字母以此類推")
+    print("可不考慮負數及浮點數的計算")
     print(m2n())    
     
-    print("team13:"
-          "Jon Snow"
-          "Robert Stark"
-          "Eddard Stark"
-          "Bran Stark"
-          "Tywin Lannister"
-          "Jamie Lannister"
-          "Ser Rodrik"
-          "Hodor"
-          "Rhaegar Targaryen")
-    print(team13())
-    
-    print("team14:"
-          "做一個樂透開獎程式"
-          "多組號碼49選6"
-          "輸入數字n"
-          ""
-          "便會跑出n組1~49中隨機挑選的六個數字")
+    print("team14:")
+    print("做一個樂透開獎程式")
+    print("多組號碼49選6")
+    print("輸入數字n")
+    print("便會跑出n組1~49中隨機挑選的六個數字")
     print(team14(int(input("please give me a number:"))))
+    
+    print("team13:")
+    print("Jon Snow")
+    print("Robert Stark")
+    print("Eddard Stark")
+    print("Bran Stark")
+    print("Tywin Lannister")
+    print("Jamie Lannister")
+    print("Ser Rodrik")
+    print("Hodor")
+    print("Rhaegar Targaryen")
+    print(team13())    
     
     
     
