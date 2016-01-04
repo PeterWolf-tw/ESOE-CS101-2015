2c8e74b060cb3e0d07ff5e47baff0d2be2fa4fd5
PeterWolf <peterwolf.wang@gmail.com>
2016-01-04 10:50:08 +0800

    最後一次作業

---
+++ Assignment_HW06.py
@@ -0,0 +1,5 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#請參考 http://interactivepython.org/runestone/static/pythonds/SortSearch/sorting.html
+#中提供的 bubble sorting 演算法，將整數列表 [5, 16, 20, 3, 8, 12, 9, 17, 20, 7] 由大排到小。
