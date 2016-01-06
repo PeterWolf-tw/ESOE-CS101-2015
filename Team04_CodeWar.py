826418fbc75a7e1da1756bbaf3a108f1edeeca3c
supercongbao <supercongbao@gmail.com>
2016-01-06 17:23:13 +0800

    finished

--- Team04_CodeWar.py
+++ Team04_CodeWar.py
@@ -210,7 +210,175 @@ for i in range(0,10):
             print(i*c,end='')
             print(" ",end='')
     print( )
+#Team_11
+def team11():
+	y = 0
+	while y == 0: 
+	    PlayerLIST = []
+	    Function = []
+	    n = 10
+	    i = 0
+	    
+	    while n > 5 or n < 2:
+	        if i == 0:
+	            n = input("請輸入玩家數（限2至5名）：")
+	        else:
+	            n = input("請重新輸入玩家數（限2至5名）：")
+	        if n.isdigit() == True:
+	            n = int(n)
+	        i += 1
+	    print("") 
+	    
+	    i = 0
+	    x = 0
+	    while i < n:
+	        print("請輸入玩家",i+1, "名稱：")
+	        Name = input()
+	        while i > x:
+	            x += 1
+	            if Name == PlayerLIST[x*2-1]:
+	                print("不允許玩家同名！")
+	                Name = input("請重新輸入玩家名稱：")
+	                x = 0
+	        x = 0
+	        PlayerLIST.append(i+1)
+	        PlayerLIST.append(Name)
+	        Function.append(Name)
+	        Function.append(1)
+	        Function.append(1)
+	        i += 1
+	    print("") 
+	    
+	    import random
+	    j = random.randint(0,n-1)
+	    z = j*2+1
+	    Function[3*j+1] = 0
+	    Function[3*j+2] = 0
+	    print ("以下數字只有玩家",PlayerLIST[z],"能看")
+	    print ("按下enter後看數字")
+	    m = input("")
+	    w = random.randint(1,36)
+	    print(w)
+	    print("往後的輸入皆由",PlayerLIST[z],"操作")
+	    print("")
+	    
+	    N = 1
+	    r = 1
+	    Bingo = 0
+	    inputLIST = ["1","2","3","return","pass"]
+	    while w > Bingo:
+	        print("目前數字為：",Bingo)
+	        print("請玩家",PlayerLIST[2*N-1],"選擇1至3的其中一數字：")
+	        print("(提醒您：此玩家尚有",Function[3*N-2],"次return功能牌",Function[3*N-1],"次pass功能牌)")
+	        a = input("")
+	        while a not in inputLIST:
+	            print ("輸入錯誤！請重新輸入：")
+	            a = input("")
+	        while inputLIST.index(a) == 3 and Function[3*N-2] == 0:
+	            print ("此玩家已經沒有return的功能牌了！請重新輸入：")
+	            a = input("")   
+	        while inputLIST.index(a) == 4 and Function[3*N-1] == 0:
+	            print ("此玩家已經沒有pass的功能牌了！請重新輸入：")
+	            a = input("")    
+	        print("")
+	        if inputLIST.index(a) < 3:
+	            Bingo += int(a)
+	        elif inputLIST.index(a) == 3:
+	            Function[3*N-1] = 0
+	            Function[3*N-2] = 0
+	            r = -r
+	        elif inputLIST.index(a) == 4:
+	            Function[3*N-1] = 0
+	            Function[3*N-2] = 0
+	        if w > Bingo:
+	            A = N        
+	        N += r
+	        if N > n:
+	            N -= n
+	        if N < 0:
+	            N += n
+	            
+	    print ("玩家",PlayerLIST[2*A-1],"lost the game!")
+	    print("")
+#Team12
+def index():
+    alllist = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+    newlist = []
+    j = 0
+    for i in alllist:
+        newlist.append((i,j))
+        j = j+1
+    return newlist
 
+def translater(inputSTR):
+    yee = index()
+    out = []
+    for i in inputSTR:
+        for j in yee:
+            if i in j :
+                out.append(j[1])
+
+            else :
+                continue
+    return out          
+
+def team12(inputSTR,a,b):
+    
+    anum = translater(inputSTR)
+    #a = 16
+    #b = 2
+    bnum = []
+    s = 0
+    j = len(anum)
+    result = ""
+    for i in anum:
+        s = s + i*(a**(j-1))
+        j = j-1
+    while(s != 0):
+        bnum.append(s%b)
+        s = s - s%b
+        s = s//b
+    bnum.reverse()
+    for i in bnum:
+        for j in index():
+            if i in j :
+                result = result + j[0]
+    
+    return result
+
+
+#Team13
+def team13():
+
+    total = ["000","100","200","300","400","500","600","700","800","900"]
+    S = [3,2,4,1,8,9]
+    L = [5,6]
+    T = [7,0]
+    sir = [(1,2),(1,3),(8,2),(2,3),(9,4),(9,3),(4,3),(6,5)]
+    name = ["non","Jon Snow","Robert Stark","Eddard Stark","Bran Stark","Tywin Lannister","Jamie Lannister","Rhaegar Targaryen","Ser Rodrik","Hodor"]
+    while True:
+        x = input ("請輸入第一人代碼:")
+        y = input ("請輸入第二人代碼:")    
+        a = int(int(x) / 100)
+        b = int(int(y) / 100)    
+        if x not in total or y not in total or x == y:
+            print("There's something wrong!")
+            continue
+            
+        else:    
+            if a in S and b in S or a in T and b in T: 
+                if (a,b) in sir:
+                    print("%s是%s的屬下"%(name[a],name[b]))
+                elif (b,a) in sir:
+                    print("%s是%s的長官"%(name[a],name[b]))
+                else:
+                    print("%s與%s是平輩"%(name[a],name[b]))
+            elif a in T or b in T:
+                print ("資料有誤無法比對")
+            else:
+                print ("%s與%s是敵人"%(name[a],name[b]))
+            break
+			
 #Team14
 def team14(inputSTR):
     import random
@@ -264,10 +432,19 @@ if __name__ == "__main__":
     
     YO=input("please enter a number:")
     print(team14(YO))    
-    
-    
-    
-    
+    print("Team11:")
+	team11()  
+	
+	
+    print("Team12:")
+    t12a = int(input("進位m>>>"))
+    t12b = int(input("進位n>>>"))
+   # print(translater("16ZEQBA"))
+    print(team12("16ZEQBA",t12a,t12b))  
+	
+	
+    print("Team13:")
+	team13()
     
     
     
