f3d0c0a9a03fd762b031f7aec3cbf915ef5bcd59
HowardChuang12 <t689222289vn@gmail.com>
2016-01-22 15:54:57 +0800

    答案是cpp 在另一個檔案

---
+++ Team10_Answer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+
+#答案如 team10.cpp
+    
+    
+# team01 pass, 對齊的有點不一樣
+# team02 fail
+# team03 pass, 對齊的有點不一樣
+# team04 pass, 對齊的有點不一樣
+# team05 fail
+# team06 pass, 對齊的有點不一樣
+# team07 fail 不是要練習print= =
+# team08 pass, 對齊的有點不一樣
+# team09 pass, 對齊的有點不一樣
+# team11 pass
+# team12 pass
+# team13 pass
+# team14 fail 不是要練習print= =
+
+
